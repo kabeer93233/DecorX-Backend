fc70b54e4078d62df40504b2bd44ccf0218b85aa
Muhammad Kabeer <mk1748407@gmail.com>
2026-06-08 06:59:05 +0500

    fix vercel deploy

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-    app.enableCors({
+
+  app.enableCors({
     origin: '*',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   });
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist : true,
-    forbidNonWhitelisted : true
-  }))
-  // await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
